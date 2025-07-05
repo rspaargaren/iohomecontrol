600ccc3a107aeb9a35568e89227cd0a8132003c9
rspaargaren <github@speijkers.nl>
2025-07-05 15:14:28 +0200

    Publish state on MQTT commands

--- include/interact.h
+++ include/interact.h
@@ -243,9 +243,16 @@ inline void onMqttDisconnect(AsyncMqttClientDisconnectReason) {
         std::transform(payloadStr.begin(), payloadStr.end(), payloadStr.begin(), ::tolower);
         t.push_back(payloadStr);
         t.push_back(it->description);
+        std::string stateTopic = "iown/" + id + "/state";
 
-        if (payloadStr == "open") IOHC::iohcRemote1W::getInstance()->cmd(IOHC::RemoteButton::Open, &t);
-        else if (payloadStr == "close") IOHC::iohcRemote1W::getInstance()->cmd(IOHC::RemoteButton::Close, &t);
+        if (payloadStr == "open") {
+          IOHC::iohcRemote1W::getInstance()->cmd(IOHC::RemoteButton::Open, &t);
+          mqttClient.publish(stateTopic.c_str(), 0, true, "open");
+        }
+        else if (payloadStr == "close") {
+          IOHC::iohcRemote1W::getInstance()->cmd(IOHC::RemoteButton::Close, &t);
+          mqttClient.publish(stateTopic.c_str(), 0, true, "closed");
+        }
         else if (payloadStr == "stop") IOHC::iohcRemote1W::getInstance()->cmd(IOHC::RemoteButton::Stop, &t);
         else if (payloadStr == "vent") IOHC::iohcRemote1W::getInstance()->cmd(IOHC::RemoteButton::Vent, &t);
         else if (payloadStr == "force") IOHC::iohcRemote1W::getInstance()->cmd(IOHC::RemoteButton::ForceOpen, &t);
