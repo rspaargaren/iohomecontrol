4db9c23db2f15e0e37e4481649508d45822176d0
rspaargaren <github@speijkers.nl>
2025-07-12 20:29:20 +0200

    merge changes

--- include/web_server_handler.h
+++ include/web_server_handler.h
@@ -4,9 +4,14 @@
 #include <Arduino.h>
 
 // Forward declaration if ESPAsyncWebServer is used
-class ESPAsyncWebServer; 
+class ESPAsyncWebServer;
 
+#if defined(WEBSERVER)
 void setupWebServer();
 void loopWebServer(); // If any loop processing is needed for the web server
+#else
+inline void setupWebServer() {}
+inline void loopWebServer() {}
+#endif
 
 #endif // WEB_SERVER_HANDLER_H
