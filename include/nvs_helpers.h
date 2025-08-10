1ed070f5d9b6813d0527d1f2d1409002ad0eb401
rspaargaren <github@speijkers.nl>
2025-08-10 12:41:43 +0200

    chore: remove default MQTT credentials

--- include/nvs_helpers.h
+++ include/nvs_helpers.h
@@ -2,9 +2,13 @@
 #define NVS_HELPERS_H
 
 #include <iohcPacket.h>
+#include <string>
 
 bool nvs_init();
 bool nvs_read_sequence(const IOHC::address addr, uint16_t *sequence);
 void nvs_write_sequence(const IOHC::address addr, uint16_t sequence);
+
+bool nvs_read_string(const char *key, std::string &value);
+void nvs_write_string(const char *key, const std::string &value);
 
 #endif // NVS_HELPERS_H
